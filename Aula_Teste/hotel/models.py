3a554c4d8d72841cc7e7b9be67a4f0ab543ec789
lerpaiva <leticiarpaival@gmail.com>
2024-05-14 13:17:47 -0300

    up atividade 2

---
+++ Aula_Teste/hotel/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+
+#EM MINUSCULO É O QUE APARECE NA HORA DA INTERFACE, O MAÍUSCULO É O QUE FICARÁ NO BANCO DE DADOS
+TIPOS_QUARTOS = (
+    ("SOLTEIRO", "Solteiro"),
+    ("CASAL", "Casal"),
+    ("CONFORTO", "Conforto"),
+    ("LUXO", "Luxo")
+)
+
+#CLASS HOTEL, DUAS PROPRIEDADES COM TAMANHO MÁXIMO, CHARFIELD ESPERA POUCA COISA E TEXT PERMITE MAIS
+class Hotel(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField(max_length=500)
+    logo = models.ImageField(upload_to="logo/")
+
+    def __str__(self):
+        return self.titulo
+
+class Quarto(models.Model):
+    tipo = models.CharField(max_length=15, choices=TIPOS_QUARTOS)
+    disponibilidade = models.IntegerField()
+    valor = models.FloatField(max_length=4)
+    descricao = models.TextField(max_length=255)
+    foto = models.ImageField(upload_to="Foto_Quarto/")
+
+    def __str__(self):
+        return self.tipo
+
